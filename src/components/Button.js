b19fe9a042bd4d3088654592088f82f820e279dd
taurusboong2 <taurusboong2@gmail.com>
2022-01-26 17:48:32 +0900

    scss practice

---
+++ src/components/Button.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import classNames from 'classnames';
+import './Button.scss';
+
+function Button({ children, size, color, outline }) {
+  return (
+  <button className={
+    classNames('Button', size, color, { outline })
+  }>
+    {children}
+  </button>
+  );
+}
+
+Button.defalutProps = {
+  size: 'medium',
+  color: 'blue'
+}
+
+export default Button;
